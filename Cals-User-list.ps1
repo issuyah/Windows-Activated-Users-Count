9f8a5784139f7d982168b9a2739389992567d2f4
issuyah <benoitanseaumepro@gmail.com>
2023-10-17 11:12:52 +0200

    Create Cals-User-list.ps1

---
+++ Cals-User-list.ps1
@@ -0,0 +1,16 @@
+﻿# Import the ActiveDirectory module
+Import-Module ActiveDirectory
+
+# Get all active users in the domain
+$users = Get-ADUser -Filter {Enabled -eq $True -and ObjectClass -eq "user"} -Properties SamAccountName
+
+# Initialize a hashtable to store the unique users
+$uniqueUsers = @{}
+
+# Loop through the users and add them to the hashtable
+foreach ($user in $users) {
+    $uniqueUsers[$user.SamAccountName] = $true
+}
+
+# Output the number of unique users
+Write-Output $($uniqueUsers.Count)
